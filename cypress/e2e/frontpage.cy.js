adf9bd163226ba2d8f302eb89aadf834f235e67f
Sebastian Sanabria Garcia <sebastian.sanabriagarcia@emeal.nttdata.com>
2022-11-09 21:36:25 -0500

    Delete playwright pipeline.yml

--- cypress/e2e/frontpage.cy.js
+++ cypress/e2e/frontpage.cy.js
@@ -1,13 +1,13 @@
 /* global cy describe it  */
 
 describe('Alimentatec', () => {
-    it('Frontpage can be opened ', () => {
+    it('Frontpage can be opened and contain Vite', () => {
         cy.visit('http://localhost:5173/')
         cy.contains('Vite')
     });
 
-    it('Frontpage can be opened ', () => {
+    it('Frontpage can be opened and contain React ', () => {
         cy.visit('http://localhost:5173/')
-        cy.contains('?')
+        cy.contains('React')
     });
 });
